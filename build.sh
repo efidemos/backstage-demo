8b030c326b116ff121997db91479cad5bef75666
Daniel Vaskivaara <daniel.vaskivaara@eficode.com>
2024-10-18 15:16:26 +0300

    working local containerized build

--- build.sh
+++ build.sh
@@ -1,6 +1,41 @@
-#!/bin/bash
+#!/bin/bash -x
+
+POSTGRES_HOST=pgsql-backstage
+POSTGRES_PASSWORD=abc123
 
 # Change to src directory
-cd src || exit 1  # Exit if the src folder does not exist
+pushd src || exit 1  # Exit if the src folder does not exist
 
+yarn install --immutable
+
+yarn tsc
+
+pushd packages/backend
+yarn build:backend --config ../../app-config.yaml --config ../../app-config.production.yaml
+
+# return to root folder
+popd && popd
+
+docker network create -d bridge backstage 2>/dev/null
+
+# Build Docker image
 docker image build . -f packages/backend/Dockerfile --tag backstage
+
+# run Postgres
+docker run \
+  -d \
+  -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+  --name "$POSTGRES_HOST" \
+  --network=backstage \
+  postgres:17-alpine
+
+sleep 10
+
+# Run Backstage
+docker run \
+  -e POSTGRES_HOST="$POSTGRES_HOST" \
+  -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+  -it \
+  --network=backstage \
+  -p 7007:7007 \
+  backstage
